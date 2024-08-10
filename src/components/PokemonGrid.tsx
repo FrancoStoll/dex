9e99bb2a915397309de39a364422d3ad8daedab3
FrancoStoll <francostoll@gmail.com>
2024-08-10 19:09:16 -0300

    grid min-h

--- src/components/PokemonGrid.tsx
+++ src/components/PokemonGrid.tsx
@@ -11,7 +11,7 @@ export const PokemonGrid = ({ pokemons, isLoading }: Props) => {
   if (!pokemons) return <Skeleton />;
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 place-items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 place-items-center min-h-screen">
         {pokemons.map((pokemon) => (
           <PokemonCard key={pokemon.id} {...pokemon} />
         ))}
