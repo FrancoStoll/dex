579dcedd34c95bef3034b27344d20411881e9e79
FrancoStoll <francostoll@gmail.com>
2024-08-10 19:15:09 -0300

    remove useEffect offset

--- src/components/PokemonGrid.tsx
+++ src/components/PokemonGrid.tsx
@@ -13,7 +13,7 @@ export const PokemonGrid = ({ pokemons, isLoading }: Props) => {
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 place-items-center min-h-screen">
         {pokemons.map((pokemon) => (
-          <PokemonCard key={pokemon.id} {...pokemon} />
+          <PokemonCard key={pokemon.name} {...pokemon} />
         ))}
       </div>
 
