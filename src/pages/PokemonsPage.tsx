2594f3f8d26b5cf07558563670f9e19764c2f09e
FrancoStoll <francostoll@gmail.com>
2024-08-10 19:08:43 -0300

    min-h-screen grid

--- src/pages/PokemonsPage.tsx
+++ src/pages/PokemonsPage.tsx
@@ -22,6 +22,10 @@ export const PokemonsPage = () => {
 
   useEffect(() => {
     fetchData();
+  }, [offset]);
+
+  useEffect(() => {
+    fetchData();
     window.addEventListener("scroll", handleScroll);
 
     () => {
